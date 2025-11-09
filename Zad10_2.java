04c715e788bb7edd647db7dc703d86526dd3f910
ichimaxx <wyso14@gmail.com>
2025-11-09 15:41:54 -0600

    9/11/2025

---
+++ Zad10_2.java
@@ -0,0 +1,58 @@
+import static myutils.Skrocenie_Print.*;
+
+interface Selector {
+	boolean end();
+	Object current();
+	void next();
+}
+public class Zad10_2 {
+	private Object[] items;
+	private int next = 0;
+	public Zad10_2(int size) { 
+		items = new Object[size]; 
+	}
+	public void add(Object x) {
+		if(next < items.length)
+			items[next++] = x;
+	}
+	private class Koniec {
+		String k = "koniec";
+		Koniec(String k) { 
+			this.k = k;
+		}
+		@Override
+		public String toString() { 
+			return k;
+		}
+	}	
+	private class SequenceSelector implements Selector {
+		private int i = 0;
+		public boolean end() { 
+			return i == items.length; 
+		}
+		public Object current() { 
+			return items[i]; 
+		}
+		public void next() { 
+			if(i < items.length) i++; 
+		}
+	}
+	public Selector selector() {
+		return new SequenceSelector();
+	}
+	public static void main(String[] args) {
+		Zad10_2 sequence = new Zad10_2(15);
+		for(int i = 0; i < 10; i++)
+			sequence.add(Integer.toString(i));
+		Selector selector = sequence.selector();
+		sequence.add(sequence.new Koniec("koniec23"));
+		sequence.add(sequence.new Koniec("a pozniej"));
+		sequence.add(sequence.new Koniec("inny koniec"));
+		sequence.add(sequence.new Koniec("chyba..."));
+		sequence.add(sequence.new Koniec("albo nie..."));
+		while(!selector.end()) {
+			System.out.print(selector.current() + " ");
+			selector.next();
+		}
+	}
+}
---
+++ Zad10_3.java
@@ -0,0 +1,31 @@
+import static myutils.Skrocenie_Print.*;
+
+public class Zad10_3 {
+	private String p;
+	
+	public Zad10_3(String p) {
+		this.p = p;
+	}
+	class Inner {
+		private int f = 25;
+		public int value(){
+			return f;
+		}
+		@Override
+		public String toString(){
+			return p;
+		}
+	}
+	public Inner inner() {
+		return new Inner();
+	}
+
+	
+	public static void main(String[] args) {
+
+		Zad10_3 g = new Zad10_3("lol");
+		Zad10_3.Inner k = g.inner();
+		println(k.value());
+		println(k); // dlatego to dziala bo k.value wybiera przeciazenie, a samo k wybiera przeciazenie String.valueof(k) ktory z kolei wywoluje k.toString(); czyli OVERRIDED METHOD. jest to powiazane z automatycznym wybieraniem typów przez metode println
+	}
+}
