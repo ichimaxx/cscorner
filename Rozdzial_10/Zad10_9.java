641fc52f31135a857b9e5da0da107036faf4e24b
ichimaxx <wyso14@gmail.com>
2025-11-12 22:15:42 -0600

    12/11/2025

---
+++ Rozdzial_10/Zad10_9.java
@@ -0,0 +1,22 @@
+import static myutils.Skrocenie_Print.*;
+
+
+interface Parcel6 {
+	default void speak() {
+		println("speak() z interface");
+	}
+}
+	
+public class Zad10_9 {
+	public Parcel6 parcel6() {
+		class Wewnetrzna implements Parcel6 {}
+		return new Wewnetrzna();
+	}
+	public static void main (String[] args) {
+		Zad10_9 o = new Zad10_9();
+		Parcel6 op = o.parcel6();
+		op.speak();
+	}
+}
+		
+	
