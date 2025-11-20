0a87e7d374e16fa2aa2a4bcc93f31bbdc6f491b8
ichimaxx <wyso14@gmail.com>
2025-11-19 22:54:42 -0600

    19/11/2025

---
+++ Rozdzial_10/Zad10_25.java
@@ -0,0 +1,28 @@
+import controller.*;
+
+public class Zad10_25 {
+	public static void main(String[] args) {
+		GreenhouseControls2 ab = new GreenhouseControls2();
+		// Instead of hard-wiring, you could parse
+		// configuration information from a text file here:
+		ab.addEvent(ab.new Bell(900));
+		Event[] eventList = {
+			ab.new ThermostatNight(0),
+			ab.new LightOn(200),
+			ab.new LightOff(400),
+			ab.new FanOn(400),
+			ab.new FanOff(730),
+			ab.new MistOn(100),
+			ab.new MistOff(830),
+			ab.new WaterOn(600),
+			ab.new WaterOff(800),
+			ab.new ThermostatDay(1400)
+		};
+		ab.addEvent(ab.new Restart(2000, eventList));
+		if(args.length == 1)
+			ab.addEvent(
+				new GreenhouseControls2.Terminate(
+					Integer.parseInt(args[0])));
+		ab.run();
+	}
+}
