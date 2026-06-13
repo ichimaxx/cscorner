f579c176f105e7943cf7dc2316223d40e96fd256
ichimaxx <wyso14@gmail.com>
2026-06-12 23:27:01 -0500

    12/6/2026

---
+++ myutils/Test.java
@@ -0,0 +1,9 @@
+package myutils;
+
+public abstract class Test<C> {
+    String name;
+    public Test(String name) { this.name = name; }
+    // Override this method for different tests.
+    // Returns actual number of repetitions of test.
+    abstract int test(C container, TestParam tp);
+}
--- myutils/Vector.java
+++
@@ -1,8 +0,0 @@
-package myutils;
-import static myutils.Skrocenie_Print.*;
-
-public class Vector { 
-  public Vector() { 
-    System.out.println("net.mindview.simple.Vector"); 
-  } 
-} ///:~
