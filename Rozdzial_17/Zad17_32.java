fc2196405f4c711d945a20c315a86bb3435ddda8
ichimaxx <wyso14@gmail.com>
2026-06-14 13:54:54 -0500

    14/6/2026

---
+++ Rozdzial_17/Zad17_32.java
@@ -0,0 +1,189 @@
+import java.util.*;
+import myutils.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+/*
+Exercise 32: (2) Repeat the previous exercise for a container of int, and compare the
+performance to an ArrayList<Integer>. In your performance comparison, include the
+process of incrementing each object in the container.
+*/
+class NowyConInt {
+    private int index = 0;
+    private int[] ara = new int[1];
+    public int size() {
+        return index;
+    }
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(ara, index));
+    }
+    public boolean add(int o) {
+        int indeka = 0;
+        if (index == ara.length) {
+            //tablica jest powiększana kilka razy, aby nie kopiować jej przy każdym add()
+            int[] aras = new int[ara.length * 5];
+            for (int ok : ara) {
+                aras[indeka] = ok;
+                indeka++;
+            }
+            ara = aras;
+        }
+        ara[index] = o;
+        index++;
+        return true;
+    }
+    public int get(int indeks) {
+        if (indeks >= index || indeks < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return ara[indeks];
+    }
+    public void clear() {
+        index = 0;
+    }
+    public int incr(int k) {
+        if(k >= index || k < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        ara[k] = ara[k] + 1;
+        return ara[k];
+
+    }
+}
+public class Zad17_32 {
+    static Random rand = new Random();
+    static int reps = 1000;
+    static List<Test<List<Integer>>> tests =
+            new ArrayList<Test<List<Integer>>>();
+    // osobny test dla nowo stworzonego kontenera NowyCon
+    static List<Test<NowyConInt>> conTests =
+            new ArrayList<Test<NowyConInt>>();
+
+    static {
+        tests.add(new Test<List<Integer>>("add") {
+            int test(List<Integer> list, TestParam tp) {
+                int loops = tp.loops;
+                int listSize = tp.size;
+                Integer[] arrayes = Generated.array(Integer.class, new CountingGenerator.Integer(), listSize);
+                for(int i = 0; i < loops; i++) {
+                    list.clear();
+                    for(int j = 0; j < listSize; j++)
+                        list.add(arrayes[j]);
+                }
+                return loops * listSize;
+            }
+        });
+        tests.add(new Test<List<Integer>>("incr") {
+            int test(List<Integer> list, TestParam tp) {
+                int loops = tp.loops;
+                int listSize = tp.size;
+                for(int i = 0; i < loops; i++) {
+                    for(int j = 0; j < listSize; j++)
+                        list.set(j, list.get(j) + 1);
+                }
+                return loops * listSize;
+            }
+        });
+        conTests.add(new Test<NowyConInt>("add") {
+            int test(NowyConInt list, TestParam tp) {
+                int loops = tp.loops;
+                int listSize = tp.size;
+                Integer[] arrayes = Generated.array(Integer.class, new CountingGenerator.Integer(), listSize);
+                for(int i = 0; i < loops; i++) {
+                    list.clear();
+                    for(int j = 0; j < listSize; j++)
+                        list.add(arrayes[j]);
+                }
+                return loops * listSize;
+            }
+        });
+        conTests.add(new Test<NowyConInt>("incr") {
+            int test(NowyConInt list, TestParam tp) {
+                int loops = tp.loops;
+                int listSize = tp.size;
+                for(int i = 0; i < loops; i++) {
+                    for(int j = 0; j < listSize; j++)
+                        list.incr(j);
+                }
+                return loops * listSize;
+            }
+        });
+        tests.add(new Test<List<Integer>>("get") {
+            int test(List<Integer> list, TestParam tp) {
+                int loops = tp.loops * reps;
+                int listSize = list.size();
+                for(int i = 0; i < loops; i++)
+                    list.get(rand.nextInt(listSize));
+                return loops;
+            }
+        });
+        conTests.add(new Test<NowyConInt>("get") {
+            int test(NowyConInt list, TestParam tp) {
+                int loops = tp.loops * reps;
+                int listSize = list.size();
+                for(int i = 0; i < loops; i++)
+                    list.get(rand.nextInt(listSize));
+                return loops;
+            }
+        });
+    }
+
+    static class ListTester extends Tester<List<Integer>> {
+        public ListTester(List<Integer> container,
+                          List<Test<List<Integer>>> tests) {
+            super(container, tests);
+        }
+
+        // Fill to the appropriate size before each test:
+        @Override
+        protected List<Integer> initialize(int size){
+            Integer[] arrayes = Generated.array(Integer.class, new CountingGenerator.Integer(), size);
+            container.clear();
+            container.addAll(Arrays.asList(arrayes));
+            return container;
+        }
+
+        // Convenience method:
+        public static void run(List<Integer> list,
+                               List<Test<List<Integer>>> tests) {
+            new Zad17_32.ListTester(list, tests).timedTest();
+        }
+    }
+    static class ConTester extends Tester<NowyConInt> {
+        public ConTester(NowyConInt container,
+                         List<Test<NowyConInt>> tests) {
+            super(container, tests);
+        }
+
+        // Fill to the appropriate size before each test:
+        @Override
+        protected NowyConInt initialize(int size){
+            container.clear();
+            Integer[] arrayes = Generated.array(Integer.class, new CountingGenerator.Integer(), size);
+            for (int i = 0; i < size;i++) {
+                container.add(arrayes[i]);
+            }
+            return container;
+        }
+
+        // Convenience method:
+        public static void run(NowyConInt list,
+                               List<Test<NowyConInt>> tests) {
+            new Zad17_32.ConTester(list, tests).timedTest();
+        }
+    }
+    public static void main(String[] args) {
+        Tester.defaultParams= TestParam.array(
+                10, 5000, 100, 5000, 1000, 1000, 10000, 200);
+        if(args.length > 0)
+            Tester.defaultParams = TestParam.array(args);
+        Tester.fieldWidth = 12;
+
+        Zad17_32.ConTester.run(new NowyConInt(), conTests);
+        Zad17_32.ListTester.run(new ArrayList<Integer>(), tests);
+    }
+}
+//incr zrobione jako metoda bezpośrednio z kontenera jest szybsze, ponieważ od razu pracuje na tablicy int[].
+//w przypadku ArrayList używane są dodatkowo metody get, aby wziąć wartość i set, aby ustawić nową wartość,
+//co dodatkowo wymaga boxing i unboxing wartości
---
+++ Rozdzial_17/Zad17_33.java
@@ -0,0 +1,297 @@
+import java.util.*;
+import myutils.*;
+/*
+Exercise 33: (5) Create a FastTraversalLinkedList that internally uses a LinkedList
+for rapid insertions and removals, and an ArrayList for rapid traversals and get( )
+operations. Test it by modifying ListPerformance.java.
+*/
+class FastTraversalLinkedList<E> extends AbstractList<E> {
+    //LinkedList służy jako główna struktura do szybkiego add(), remove()
+    //arrayList jest cache do get() i przechodzenia po liście
+    private LinkedList<E> kk = new LinkedList<E>();
+    private ArrayList<E> kz = new ArrayList<E>();
+    private boolean changed = true;
+    public void synchro() {
+        if(changed) {
+            kz = new ArrayList<E>(kk);
+            changed = false;
+        }
+    }
+    @Override
+    public E set(int index, E element) {
+        E old = kk.set(index, element);
+        changed = true;
+        return old;
+    }
+    @Override
+    public boolean add(E e) {
+        kk.add(e);
+        changed = true;
+        return true;
+    }
+    @Override
+    public void add(int index, E e) {
+        kk.add(index, e);
+        changed = true;
+    }
+    @Override
+    public E get(int index) {
+        synchro();
+        return kz.get(index);
+    }
+
+    @Override
+    public int size() {
+        return kk.size();
+    }
+    @Override
+    public E remove(int index) {
+        E old = kk.remove(index);
+        changed = true;
+        return old;
+    }
+    @Override
+    public void clear() {
+        kk.clear();
+        kz.clear();
+        changed = false;
+    }
+    //wymagane, aby test "iteradd" działał po iteratorze LinkedList inaczej będzie przechodzić po indeksach,
+    //w czym LinkedList nie jest dobra(jest powolna)
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        final ListIterator<E> it = kk.listIterator(index);
+        return new ListIterator<E>() {
+
+            @Override
+            public boolean hasNext() {
+                return it.hasNext();
+            }
+
+            @Override
+            public E next() {
+                return it.next();
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return it.hasPrevious();
+            }
+
+            @Override
+            public E previous() {
+                return it.previous();
+            }
+
+            @Override
+            public int nextIndex() {
+                return it.nextIndex();
+            }
+
+            @Override
+            public int previousIndex() {
+                return it.previousIndex();
+            }
+
+            @Override
+            public void remove() {
+                it.remove();
+                changed = true;
+            }
+
+            @Override
+            public void set(E e) {
+                it.set(e);
+                changed = true;
+            }
+
+            @Override
+            public void add(E e) {
+                it.add(e);
+                changed = true;
+            }
+        };
+    }
+    //używa iterator() z ArrayList
+    @Override
+    public Iterator<E> iterator() {
+        synchro();
+        return kz.iterator();
+    }
+}
+public class Zad17_33 {
+    static Random rand = new Random();
+    static int reps = 1000;
+    static List<Test<List<Integer>>> tests =
+            new ArrayList<Test<List<Integer>>>();
+    static List<Test<LinkedList<Integer>>> qTests =
+            new ArrayList<Test<LinkedList<Integer>>>();
+    static {
+        tests.add(new Test<List<Integer>>("add") {
+            int test(List<Integer> list, TestParam tp) {
+                int loops = tp.loops;
+                int listSize = tp.size;
+                for(int i = 0; i < loops; i++) {
+                    list.clear();
+                    for(int j = 0; j < listSize; j++)
+                        list.add(j);
+                }
+                return loops * listSize;
+            }
+        });
+        tests.add(new Test<List<Integer>>("get") {
+            int test(List<Integer> list, TestParam tp) {
+                int loops = tp.loops * reps;
+                int listSize = list.size();
+                for(int i = 0; i < loops; i++)
+                    list.get(rand.nextInt(listSize));
+                return loops;
+            }
+        });
+        tests.add(new Test<List<Integer>>("set") { int test(List<Integer> list, TestParam tp) {
+            int loops = tp.loops * reps;
+            int listSize = list.size();
+            for(int i = 0; i < loops; i++)
+                list.set(rand.nextInt(listSize), 47);
+            return loops;
+        }
+        });
+        tests.add(new Test<List<Integer>>("iteradd") {
+            int test(List<Integer> list, TestParam tp) {
+                final int LOOPS = 1000000;
+                int half = list.size() / 2;
+                ListIterator<Integer> it = list.listIterator(half);
+                for(int i = 0; i < LOOPS; i++)
+                    it.add(47);
+                return LOOPS;
+            }
+        });
+        tests.add(new Test<List<Integer>>("insert") {
+            int test(List<Integer> list, TestParam tp) {
+                int loops = tp.loops;
+                for(int i = 0; i < loops; i++)
+                    list.add(5, 47); // Minimize random-access cost
+                return loops;
+            }
+        });
+        tests.add(new Test<List<Integer>>("remove") {
+            int test(List<Integer> list, TestParam tp) {
+                int loops = tp.loops;
+                int size = tp.size;
+                for(int i = 0; i < loops; i++) {
+                    list.clear();
+                    list.addAll(new CountingIntegerList(size));
+                    while(list.size() > 5)
+                        list.remove(5); // Minimize random-access cost
+                }
+                return loops * size;
+            }
+        });
+        // Tests for queue behavior:
+        qTests.add(new Test<LinkedList<Integer>>("addFirst") {
+            int test(LinkedList<Integer> list, TestParam tp) {
+                int loops = tp.loops;
+                int size = tp.size;
+                for(int i = 0; i < loops; i++) {
+                    list.clear();
+                    for(int j = 0; j < size; j++)
+                        list.addFirst(47);
+                }
+                return loops * size;
+            }
+        });
+        qTests.add(new Test<LinkedList<Integer>>("addLast") {
+            int test(LinkedList<Integer> list, TestParam tp) {
+                int loops = tp.loops;
+                int size = tp.size;
+                for(int i = 0; i < loops; i++) {
+                    list.clear();
+                    for(int j = 0; j < size; j++)
+                        list.addLast(47);
+                }
+                return loops * size;
+            } });
+        qTests.add(
+                new Test<LinkedList<Integer>>("rmFirst") {
+                    int test(LinkedList<Integer> list, TestParam tp) {
+                        int loops = tp.loops;
+                        int size = tp.size;
+                        for(int i = 0; i < loops; i++) {
+                            list.clear();
+                            list.addAll(new CountingIntegerList(size));
+                            while(list.size() > 0)
+                                list.removeFirst();
+                        }
+                        return loops * size;
+                    }
+                });
+        qTests.add(new Test<LinkedList<Integer>>("rmLast") {
+            int test(LinkedList<Integer> list, TestParam tp) {
+                int loops = tp.loops;
+                int size = tp.size;
+                for(int i = 0; i < loops; i++) {
+                    list.clear();
+                    list.addAll(new CountingIntegerList(size));
+                    while(list.size() > 0)
+                        list.removeLast();
+                }
+                return loops * size;
+            }
+        });
+    }
+    static class ListTester extends Tester<List<Integer>> {
+        public ListTester(List<Integer> container,
+                          List<Test<List<Integer>>> tests) {
+            super(container, tests);
+        }
+        // Fill to the appropriate size before each test:
+        @Override protected List<Integer> initialize(int size){
+            container.clear();
+            container.addAll(new CountingIntegerList(size));
+            return container;
+        }
+        // Convenience method:
+        public static void run(List<Integer> list,
+                               List<Test<List<Integer>>> tests) {
+            new ListTester(list, tests).timedTest();
+        }
+    }
+    public static void main(String[] args) {
+        if(args.length > 0)
+            Tester.defaultParams = TestParam.array(args);
+        // Can only do these two tests on an array:
+        Tester<List<Integer>> arrayTest =
+                new Tester<List<Integer>>(null, tests.subList(1, 3)){
+                    // This will be called before each test. It
+                    // produces a non-resizeable array-backed list:
+                    @Override protected
+                    List<Integer> initialize(int size) {
+                        Integer[] ia = Generated.array(Integer.class,
+                                new CountingGenerator.Integer(), size);
+                        return Arrays.asList(ia);
+                    }
+                };
+        arrayTest.setHeadline("Array as List");
+        arrayTest.timedTest();
+        Tester.defaultParams= TestParam.array(
+                10, 5000, 100, 5000, 1000, 1000, 10000, 200);
+        if(args.length > 0)
+            Tester.defaultParams = TestParam.array(args);
+        ListTester.run(new ArrayList<Integer>(), tests);
+        ListTester.run(new LinkedList<Integer>(), tests);
+        ListTester.run(new Vector<Integer>(), tests);
+        //nowa lista
+        ListTester.run(new FastTraversalLinkedList<Integer>(), tests);
+        Tester.fieldWidth = 12;
+        Tester<LinkedList<Integer>> qTest =
+                new Tester<LinkedList<Integer>>(
+                        new LinkedList<Integer>(), qTests);
+        qTest.setHeadline("Queue tests");
+        qTest.timedTest();
+    }
+}
+//FastTraversalLinkedList łączy LinkedList i ArrayList:
+//LinkedList jest główną strukturą dla add/remove,
+//a ArrayList służy jako cache dla szybkiego get()
+//listIterator() opiera się na LinkedList, żeby iteradd nie musiał pracować przez indeksy.
+//set() nadal jest wolniejsze bo zmienia dane w LinkedList.
