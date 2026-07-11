3f2ab364dc6a0bbef1df8aca444c8fcc042d4bed
ichimaxx <wyso14@gmail.com>
2026-07-10 22:26:07 -0500

    10/7/2027

--- Rozdzial_19/Zad19_11.java
+++ Rozdzial_19/Zad19_11.java
@@ -2,13 +2,15 @@ import java.util.*;
 import static myutils.Skrocenie_Print.*;
 import myutils.*;
 /*
-Exercise 10: (7) Modify class VendingMachine (only) using EnumMap so that one
-program can have multiple instances of VendingMachine.
+Exercise 11: (7) In a real vending machine you will want to easily add and change the
+type of vended items, so the limits imposed by an enum on Input are impractical
+(remember that enums are for a restricted set of types). Modify VendingMachine.java so
+that the vended items are represented by a class instead of being part of Input, and
+initialize an Array List of these objects from a text file (using
+net.mindview.util.TextFile).
 */
-//Wszystkie możliwe wejścia do automatu, monety produkty oraz komendy specjalne
 enum Input {
     NICKEL(5), DIME(10), QUARTER(25), DOLLAR(100),
-    TOOTHPASTE(200), CHIPS(75), SODA(100), SOAP(50),
     ABORT_TRANSACTION {
         public int amount() { // Disallow
             throw new RuntimeException("ABORT.amount()");
@@ -31,15 +33,66 @@ enum Input {
         return values()[rand.nextInt(values().length - 1)];
     }
 }
+class VendItem {
+    private String name;
+    //lista wszystkich produktów w automacie, już nie jest enumami tylko obiektami klasy VendItem
+    static ArrayList<VendItem> items = new ArrayList<VendItem>();
+    private int price;
+    public VendItem(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+    public String name() {
+        return name;
+    }
+    public int amount() {
+        return price;
+    }
+    public String toString() {
+        return name;
+    }
+    //wczytuje produkty z pliku tekstowego
+    //format pliku:
+    //TOOTHPASTE,200;CHIPS,100;SODA,150;
+    //każdy produkt jest oddzielony średnikiem, a nazwa i cena produktu oddzielone przecinkiem
+    public static void loadItems(String fileName) {
+        for (String s : new TextFile(fileName, ";")) {
+            s = s.trim();
+            if(s.length() == 0)
+                continue;
+            String[] parts = s.split(",");
+            //jeśli po podziale nie ma dokładnie dwóch części,
+            // czyli nazwy i ceny, to format pliku jest niepoprawny
+            if(parts.length != 2)
+                throw new IllegalArgumentException(
+                        "Zły format produktu w " + fileName + ": " + s +
+                                ". Powinno być np. TOOTHPASTE,200;CHIPS,100;SODA,150;SOAP,50;GREENS,10;COKE,100000;");
+            items.add(new VendItem(parts[0].trim(), Integer.parseInt(parts[1].trim())));
+        }
+    }
+    //szuka produktu po nazwie w ArrayList<VendItem>
+    //np. jeśli z pliku wejściowego przyjdzie "CHIPS",
+    //metoda szuka obiektu VendItem o nazwie CHIPS
+    public static VendItem findItem (String name) {
+        for(VendItem item : items)
+            if(item.name().equals(name))
+                return item;
+        //jeżeli produktu nie znaleziono, zwraca null
+        return null;
+    }
+}
 //interfejs reprezentujący akcję, wykonywaną dla danego automatu
-interface CommandsVM { void next(Input m); }
+//przyjmuje tutaj Object(szerzej), ponieważ wejściem może być albo Input, albo VendItem
+interface CommandsVM { void next(Object  m); }
 //Category grupuje wartości Input na logiczne kategorie
 enum Category {
     MONEY(Input.NICKEL, Input.DIME, Input.QUARTER, Input.DOLLAR),
-    ITEM_SELECTION(Input.TOOTHPASTE, Input.CHIPS, Input.SODA, Input.SOAP),
+    ITEM_SELECTION(),
     QUIT_TRANSACTION(Input.ABORT_TRANSACTION),
     SHUT_DOWN(Input.STOP);
     private Input[] values;
+    Input command = null;
+
     Category(Input... types) { values = types; }
     //EnumMap pozwala szybko znaleźć kategorię dla konkretnego Input,
     //kluczem jest Input a wartością odpowiadającą mu Category
@@ -52,16 +105,19 @@ enum Category {
                 categories.put(type, c);
     }
     //zwraca kategorię dla podanego wejścia, np Quarter -> Money, Toothpaste -> item selection
-    public static Category categorize(Input input) {
-        return categories.get(input);
+    public static Category categorize(Object input) {
+        if(input instanceof VendItem)
+            return ITEM_SELECTION;
+        //rzutowanie na Input, ponieważ input jest Object, czyli szerzej niż Input
+        return categories.get((Input)input);
     }
 }
-class Zad19_10 {
+class Zad19_11 {
     //brak pól statycznych, dzięki czemu można uruchomić kilka instancji VendingMachine,
     //każdy ma własny stan kwotę i produkt
     private State state = State.RESTING;
     private int amount = 0;
-    private Input selection = null;
+    private VendItem selection = null;
     //mapa stanów automatu, klczem jest State, a wartością komenda opisująca zachowanie tego stanu
     private EnumMap<State, CommandsVM> states =
             new EnumMap<State, CommandsVM>(State.class);
@@ -74,7 +130,7 @@ class Zad19_10 {
     enum State {
         RESTING,ADDING_MONEY,DISPENSING(StateDuration.TRANSIENT),GIVING_CHANGE(StateDuration.TRANSIENT),TERMINAL;
         //true oznacza, że stan ma zostać wykonany automatycznie w pętli run()
-            private boolean isTransient = false;
+        private boolean isTransient = false;
         State() {}
         State(StateDuration trans) {
             isTransient = true;
@@ -82,13 +138,14 @@ class Zad19_10 {
     }
     //Konstruktor wypełnia EnumMap zachowaniami dla każdego stanu,
     //Dzięki temu logika stanów jest przypisana do konkretnej instancji automatu
-    public Zad19_10() {
+    public Zad19_11() {
         //stan początkowy: automat czeka na pieniądze lub STOP
         states.put(State.RESTING, new CommandsVM() {
-            public void next(Input m) {
+            public void next(Object  m) {
                 switch (Category.categorize(m)) {
                     case MONEY:
-                        amount += m.amount();
+                        //rzutowanie na Input, ponieważ m jest Object, czyli szerzej niż VendItem/Input
+                        amount += ((Input)m).amount();
                         state = State.ADDING_MONEY;
                         break;
                     case SHUT_DOWN:
@@ -100,15 +157,16 @@ class Zad19_10 {
         //Stan dodawania pieniędzy, można dorzucać monety, wybrać produkt
         //anulować transakcje albo zatrzymać automat
         states.put(State.ADDING_MONEY, new CommandsVM() {
-            public void next(Input m) {
+            public void next(Object  m) {
                 switch (Category.categorize(m)) {
                     case MONEY:
-                        amount += m.amount();
+                        amount += ((Input)m).amount();
                         break;
                     case ITEM_SELECTION:
-                        selection = m;
+                        //rzutowanie na VendItem, ponieważ m jest Object, czyli szerzej niż VendItem/Input
+                        selection = (VendItem)m;
                         if(amount < selection.amount())
-                            print("Insufficient money for " + selection);
+                            print("Insufficient money for " + selection + " ");
                         else state = State.DISPENSING;
                         break;
                     case QUIT_TRANSACTION:
@@ -122,7 +180,7 @@ class Zad19_10 {
         });
         //Stan przejściowy: automat wydaje wydany produkt i przechodzi do wydawania reszty
         states.put(State.DISPENSING, new CommandsVM() {
-            public void next(Input m) {
+            public void next(Object  m) {
                 println("here is your " + selection);
                 amount -= selection.amount();
                 state = State.GIVING_CHANGE;
@@ -130,7 +188,7 @@ class Zad19_10 {
         });
         //Stan przejściowy: automat oddaje resztę i wraca do stanu RESTING
         states.put(State.GIVING_CHANGE, new CommandsVM() {
-            public void next(Input m) {
+            public void next(Object  m) {
                 if(amount > 0) {
                     println("Your change: " + amount);
                     amount = 0;
@@ -140,7 +198,7 @@ class Zad19_10 {
         });
         //Stan końcowy: automat zatrzymany
         states.put(State.TERMINAL, new CommandsVM() {
-            public void next(Input m) {
+            public void next(Object  m) {
                 println("Halted");
             }
         });
@@ -153,8 +211,8 @@ class Zad19_10 {
             println(amount);
     }
     //Główna pętla automatu
-    //pobiera kolejne Input z generatora i wykonuje komendę przypisaną do aktualnego stanu w EnumMap
-    void run(Generator<Input> gen) {
+    //pobiera kolejne wejście z generatora. Wejściem może być Input lub VendItem
+    void run(Generator<Object> gen) {
         while(state != State.TERMINAL) {
             //Zamiast state.next(..) szuka zachowania aktualnego stanu w mapie
             states.get(state).next(gen.next());
@@ -167,44 +225,49 @@ class Zad19_10 {
     }
     //tworzy generator wejść, losowy albo z pliku, jeżeli podano nazwę pliku w args[0]
     public static void main(String[] args) {
-        Generator<Input> gen = new RandomInputGenerator();
+        VendItem.loadItems("Rzeczy.txt");
+        Generator<Object> gen = new RandomInputGenerator();
         if(args.length == 1)
             gen = new FileInputGenerator(args[0]);
         //Stworzono konkretną instancję automatu,
         //można stworzyć więcej instancji, ponieważ pola state/amount/selection nie są już static
-        Zad19_10 vm = new Zad19_10();
+        Zad19_11 vm = new Zad19_11();
         vm.run(gen);
     }
 }
 // For a basic sanity check:
 //Generator losowych wejść do prostego testowania automatu
-class RandomInputGenerator implements Generator<Input> {
+class RandomInputGenerator implements Generator<Object> {
     public Input next() { return Input.randomSelection(); }
 }
 // Create Inputs from a file of ‘;’-separated strings:
 //Generator wejść z pliku tekstowego.
 //Plik powinien zawierać nazwy Input oddzielone średnikami, np.
 //QUARTER;QUARTER;CHIPS;STOP;
-class FileInputGenerator implements Generator<Input> {
+class FileInputGenerator implements Generator<Object> {
     private Iterator<String> input;
+
     public FileInputGenerator(String fileName) {
-        input = new TextFile(fileName , ";").iterator();
+        input = new TextFile(fileName, ";").iterator();
     }
-    public Input next() {
+
+    public Object next() {
         //jeżeli plik się skończy, bezpiecznie kończy działanie automatu
-        if(!input.hasNext())
-            return null;
-        return Enum.valueOf(Input.class, input.next().trim());
+        if (!input.hasNext())
+            return Input.STOP;
+        String token = input.next().trim();
+        try {
+            return Enum.valueOf(Input.class, token);
+        } catch(IllegalArgumentException e) {
+            VendItem item = VendItem.findItem(token);
+            if(item != null)
+                return item;
+            throw new IllegalArgumentException("Unknown input: " + token);
+        }
     }
 }
 
 /*
-Program jest przerobioną wersją przykładu VendingMachine, tak aby można było odpalić kilka instancji na raz.
-
-Najważniejsza zmiana to usunięcie stałych(static) pól state, amount i selection.
-Zachowanie stanów nie jest już zapisane bezpośrednio w enum State. Zamiast tego użyto EnumMap<State, CommandsVM>,
-gdzie kluczem jest stan a wartością obiekt wykonujący logikę tego stanu.
-
-Stany DISPENSING i GIVING_CHANGE są oznaczone jako TRANSIENT, więc wykonują się automatycznie,
-bez pobierania kolejnego wejścia.
- */
+Przerobiono VendingMachines, tak aby produktu nie były już częścią enuma Input.
+Produkty są teraz obiektami klasy VendItem i są wczytywane z pliku tekstowego Rzeczy.txt do ArrayList<VendItem>.
+*/
---
+++ Rozdzial_20/Zad20_1.java
@@ -0,0 +1,210 @@
+package Rozdzial_20;
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
+/*
+Exercise 1: (2) Implement more SQL types in the database example.
+*/
+//Adnotacja z ograniczeniami dla kolumny SQL
+//moze oznaczać np klucz główny, brak null albo unikalność
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Constraints {
+    boolean primaryKey() default false;
+    boolean allowNull() default true;
+    boolean unique() default false;
+}
+//adnotacja dla pola, które ma być kolumną tekstową SQL
+//value oznacza długość varchar np. @SQLString(30) -> VARCHAR(30)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface SQLString {
+    int value() default 0;
+    String name() default "";
+    Constraints constraints() default @Constraints;
+}
+//adnotacja dla pola, które ma byc kolumną typu INT i tak dalej z następnymi typami
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface SQLInteger {
+    String name() default "";
+    Constraints constraints() default @Constraints;
+}
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface SQLLong {
+    String name() default "";
+    Constraints constraints() default @Constraints;
+}
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface SQLDouble {
+    String name() default "";
+    Constraints constraints() default @Constraints;
+}
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface SQLShort{
+    String name() default "";
+    Constraints constraints() default @Constraints;
+}
+//Adnotacja dla całej klasy
+//oznacza, że dana klasa ma zostać potraktowana jak tabela w bazie danych.
+@Target(ElementType.TYPE) // Applies to classes only
+@Retention(RetentionPolicy.RUNTIME)
+@interface DBTable {
+    public String name() default "";
+}
+//Klasa, z której ma wygenerować CREATE TABLE
+//nazwa tabeli to Zad20_1
+@DBTable(name = "Zad20_1")
+public class Zad20_1 {
+    //pole firstName będzie kolumną VARCHAR(30)
+    @SQLString(30) String firstName;
+    //pole lastName będzie kolumną VARCHAR(50)
+    @SQLString(50) String lastName;
+    //pole age będzie kolumną INT i tak dalej...
+    @SQLInteger Integer age;
+    @SQLLong Long longtimenosee;
+    @SQLShort Short shorts;
+    @SQLDouble Double doubles;
+    @SQLString(value = 30,
+            constraints = @Constraints(primaryKey = true))
+    String handle;
+    //to pole nie ma adnotacji SQL więc nie będzie dodane w tabeli
+    static int memberCount;
+    public String getHandle() { return handle; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String toString() { return handle; }
+    public Integer getAge() { return age; }
+    public Long getSee() { return longtimenosee; }
+    public Double getDouble() { return doubles; }
+    public Short getShort() { return shorts; }
+    public static void main(String[] args) throws Exception {
+        //program wymaga podania nazwy klasy jako argumentu
+        //np.
+        //java Rozdzial_20.Zad20_1 Rozdzial_20.Zad20_1
+        if(args.length < 1) {
+            System.out.println("arguments: annotated classes");
+            System.exit(0);
+        }
+        //przechodzi po args, ponieważ może dostać kilka nazw klas
+        for(String className : args) {
+            //Class.forname() ładuje klasę po nazwie tekstowej, dzięki czemu może analizować klasę przez refleksje
+            Class<?> cl = Class.forName(className);
+            //pobiera adnotacje @DBTable z klasy
+            DBTable dbTable = cl.getAnnotation(DBTable.class);
+            //jeżeli klasa nie ma @DBTable, to nie traktujemy jej jako tabeli
+            if(dbTable == null) {
+                System.out.println(
+                        "No DBTable annotations in class " + className);
+                continue;
+            }
+            //pobiera nazwę tabeli z adnotacji @DBTable
+            String tableName = dbTable.name();
+            // If the name is empty, use the Class name:
+            if(tableName.length() < 1)
+                tableName = cl.getName().toUpperCase();
+            //Lista definicji kolumn np.
+            //FIRSTNAME VARCHAR(30)
+            //AGE INT
+            //itd....
+            List<String> columnDefs = new ArrayList<String>();
+            //przechodzi po wszystkich polach klasy, refleksja sprawdza pola nawet wtedy,
+            //kiedy program zna klasę dopiero po nazwie tekstowej
+            for(Field field : cl.getDeclaredFields()) {
+                String columnName = null;
+                //pobiera adnotacje z konkretnego pola
+                Annotation[] anns = field.getDeclaredAnnotations();
+                if (anns.length < 1)
+                    continue; // Not a db table column
+                //obsługi pól....
+                if(anns[0] instanceof SQLInteger) {
+                    SQLInteger sInt = (SQLInteger) anns[0];
+                    // Use field name if name not specified
+                    if(sInt.name().length() < 1)
+                        columnName = field.getName().toUpperCase();
+                    else
+                        columnName = sInt.name();
+                    columnDefs.add(columnName + " INT" +
+                            getConstraints(sInt.constraints()));
+                }
+                if(anns[0] instanceof SQLLong) {
+                    SQLLong sLong = (SQLLong) anns[0];
+                    // Use field name if name not specified
+                    if(sLong.name().length() < 1)
+                        columnName = field.getName().toUpperCase();
+                    else
+                        columnName = sLong.name();
+                    columnDefs.add(columnName + " LONG" +
+                            getConstraints(sLong.constraints()));
+                }
+                if(anns[0] instanceof SQLShort) {
+                    SQLShort sShort = (SQLShort) anns[0];
+                    // Use field name if name not specified
+                    if(sShort.name().length() < 1)
+                        columnName = field.getName().toUpperCase();
+                    else
+                        columnName = sShort.name();
+                    columnDefs.add(columnName + " SHORT" +
+                            getConstraints(sShort.constraints()));
+                }
+                if(anns[0] instanceof SQLDouble) {
+                    SQLDouble sDouble = (SQLDouble) anns[0];
+                    // Use field name if name not specified
+                    if(sDouble.name().length() < 1)
+                        columnName = field.getName().toUpperCase();
+                    else
+                        columnName = sDouble.name();
+                    columnDefs.add(columnName + " DOUBLE" +
+                            getConstraints(sDouble.constraints()));
+                }
+                if(anns[0] instanceof SQLString) {
+                    SQLString sString = (SQLString) anns[0];
+                    // Use field name if name not specified.
+                    if(sString.name().length() < 1)
+                        columnName = field.getName().toUpperCase();
+                    else
+                        columnName = sString.name();
+                    columnDefs.add(columnName + " VARCHAR(" +
+                            sString.value() + ")" +
+                            getConstraints(sString.constraints()));
+                }
+                }
+            //przeniesiono za pętle, bo bezsensownie po każdym for dodawał stringa
+            StringBuilder createCommand = new StringBuilder(
+                    "CREATE TABLE " + tableName + "(");
+            //dodaje każdą kolumnę do komenty CREATE TABLE
+            for(String columnDef : columnDefs)
+                createCommand.append("\n " + columnDef + ",");
+            // Remove trailing comma
+            //usuwa ostatni przecinek i dodaje zakończenie );
+            String tableCreate = createCommand.substring(
+                    0, createCommand.length() - 1) + ");";
+            System.out.println("Table Creation SQL for " +
+                    className + " is :\n" + tableCreate);
+        }
+    }
+    //metoda zamienia adnotacje @constraints na tekst SQL
+    //np. primaryKey = true daje " PRIMARY KEY"
+    private static String getConstraints(Constraints con) {
+        String constraints = "";
+        if(!con.allowNull())
+            constraints += " NOT NULL";
+        if(con.primaryKey())
+            constraints += " PRIMARY KEY";
+        if(con.unique())
+            constraints += " UNIQUE";
+        return constraints;
+    }
+}
+/*
+ćwiczenie rozbudowuje przykład z książki.
+W przykładzie były adnotacje typu SQLString i SQLInteger, tutaj zostały dodane SQLLong, SQLDouble i SQLShort.
+Program czyta klasę podaną w argumencie programu.
+Sprawdza, czy klasa ma adnotację @DBTable.
+Przechodzi po polach klasy za pomocą refleksji.
+Sprawdza adnotacje pól, np @SQLString, @SQLInteger
+Na tej podstawie buduje tekst komendy SQL CREATE TABLE
+*/
